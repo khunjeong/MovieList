5c5dc82c138f74cc47571077378062a8a01393e0
khunjeong <51750678+khunjeong@users.noreply.github.com>
2023-05-12 16:00:13 +0900

    Chore: Proxy 설정

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/movie/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_MOVIE_URL}/:path*`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
