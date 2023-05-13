5e023319a1c986ebda13ed41d575e2bf7c376b80
khunjeong <51750678+khunjeong@users.noreply.github.com>
2023-05-13 22:27:22 +0900

    Feat: 영화 검색 API 모듈 추가

--- src/services/movies.ts
+++ src/services/movies.ts
@@ -53,3 +53,42 @@ export async function getMoviesDetail({ id }: { id: number }) {
     return false;
   }
 }
+
+/**
+ * [API] GET 영화 검색 목록 불러오기
+ * @param { string | undefined} query 검색
+ * @param { number } page 페이지
+ * @param { string | undefined } url API EndPoint
+ */
+export async function getSearchMovies({ query, page, url }: { query?: string; page: number; url?: string }) {
+  const res = await Axios.get<IPaginationResponse<IMovieModel>>(url ? `${url}/3/search/movie` : `/movie/3/search/movie`, {
+    params: {
+      api_key: process.env.NEXT_PUBLIC_MOVIE_API,
+      language: 'ko',
+      page,
+      query,
+    },
+  });
+
+  return res.data;
+}
+
+/**
+ * [react-query] 영화 목록 InfinityScroll
+ * @param { string | undefined} query 검색
+ */
+export const useGetSearchMovies = ({ query }: { query?: string }) => {
+  const res = useInfiniteQuery(['getSearchMovies', query], ({ pageParam = 1 }) => getSearchMovies({ query, page: pageParam }), {
+    getNextPageParam: (lastPage) => {
+      console.log({ lastPage });
+      const nextPage = lastPage.page + 1;
+      return lastPage.results.length !== 0 ? nextPage : undefined;
+    },
+    staleTime: 10000,
+  });
+  const { data } = res;
+
+  const contents = data ? data.pages.map((page) => page.results).reduce((mergedList, currentlist) => [...mergedList, ...(currentlist || [])], []) : [];
+
+  return { ...res, contents };
+};
