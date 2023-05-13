31fe7f0bb2d84301b05f05fad5cee3fa514f87c0
khunjeong <51750678+khunjeong@users.noreply.github.com>
2023-05-13 22:19:02 +0900

    Feat: ReactQueryDevtools 추가

--- src/providers/ReactQueryProvider.tsx
+++ src/providers/ReactQueryProvider.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 type Props = {
   children: React.ReactNode;
@@ -9,5 +10,10 @@ type Props = {
 const queryClient = new QueryClient();
 
 export default function ReactQueryProvider({ children }: Props) {
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
 }
